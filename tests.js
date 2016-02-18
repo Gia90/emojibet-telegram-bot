3302dc35711664564f30953f0b0398a0dadc61d4
Gia90 <gia.90@hotmail.it>
2016-02-18 17:45:08 +0100

    simple emoji engine: fxed missing Q mapping

--- tests.js
+++ tests.js
@@ -7,6 +7,14 @@ engines["simpleEmoji"] = require('./libs/engines/simple-emoji-engine');
 
 // UGLY DIRTY TESTS
 
-var text = "ciao donato io ti amo da morire";
+var start = " ".charCodeAt(0);
+var end = "Z".charCodeAt(0);
+for(var i= start; i<=end; i++)
+{
+  engines["simpleEmoji"].convert(String.fromCharCode(i), function(err, out) { console.log(String.fromCharCode(i) + " is " + out); } )
+}
+
+
+var text = "abcdefghilmnopqrst _-?!%&(=^?)\"";
 
 engines["simpleEmoji"].convert(text, function(err, out) { console.log("\u{20E3} Converted \""+text+"\": "+out+""); } );
